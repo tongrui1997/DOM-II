2939a58502eeb48829816ac62d9e6b5331b0ba83
Yuxuan Zhang <zhangtongrui10@gmail.com>
2021-05-04 14:49:23 -0700

    DOM2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+Array.from(document.links).forEach(function(item) {
+    item.addEventListener("click", changeColor);
+    item.addEventListener("click", function(event) {
+        event.preventDefault();
+    });
+});
+
+function changeColor() {
+    this.style.color = "green";
+}
+function backColor() {
+    this.style.color = "black";
+}
+
+const header2 = document.querySelector(".intro h2");
+header2.addEventListener("mouseover", changeColor);
+header2.addEventListener("mouseout", backColor);
+
+Array.from(document.querySelectorAll("p")).forEach(item => item.addEventListener("wheel", changeSize));
+function changeSize() {
+    this.style.fontSize = "35px";
+}
